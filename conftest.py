6b786bb2b9768a0253424de8a9ccc01cc39c0b6d
Oleg Polikarpov <Vinches2193@yandex.ru>
2023-07-31 16:44:45 +0300

    Add driver_yandex

--- conftest.py
+++ conftest.py
@@ -1,10 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture(scope="function")
 def driver():
     driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(scope="function")
+def driver_yandex():
+    options = webdriver.ChromeOptions()
+    service = Service(executable_path=fr"C:\Users\Фантомас\PycharmProjects\Lookdecor\resource\yandexdriver.exe")
+    driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
