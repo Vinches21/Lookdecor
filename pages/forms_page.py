cefdddb49c89ec00966f5bb9e086846ef6fb2f52
Oleg Polikarpov <Vinches2193@yandex.ru>
2023-08-23 13:33:25 +0300

    Change title

--- pages/forms_page.py
+++ pages/forms_page.py
@@ -8,13 +8,13 @@ class CallbackForm(BasePage):
     locators = CallbackLocators()
 
     def filling_callback_form(self):
-        self.element_is_visible(self.locators.BUTTON_ORDER_A_CALL).click()
-        self.element_is_visible(self.locators.NAME).send_keys("Иванов Тест")
-        self.element_is_visible(self.locators.PHONE).click()
-        self.element_is_visible(self.locators.PHONE).send_keys(random.randint(9000000000, 9999999999))
-        self.element_is_visible(self.locators.TIME).send_keys(random.randint(9, 21))
-        lst = [self.element_is_visible(self.locators.DEPARTAMENT_WALLPAPER),
-               self.element_is_visible(self.locators.DEPARTAMENT_PLATE)]
+        self.elements_is_visible(self.locators.BUTTON_ORDER_A_CALL).click()
+        self.elements_is_visible(self.locators.NAME).send_keys("Иванов Тест")
+        self.elements_is_visible(self.locators.PHONE).click()
+        self.elements_is_visible(self.locators.PHONE).send_keys(random.randint(9000000000, 9999999999))
+        self.elements_is_visible(self.locators.TIME).send_keys(random.randint(9, 21))
+        lst = [self.elements_is_visible(self.locators.DEPARTAMENT_WALLPAPER),
+               self.elements_is_visible(self.locators.DEPARTAMENT_PLATE)]
         random.choice(lst).click()
         # self.element_is_visible(self.locators.BUTTON_CALL_ME_BACK).click()
 
